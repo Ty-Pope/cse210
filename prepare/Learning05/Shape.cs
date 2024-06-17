fb3897e574c5e5d8d13d3a4c19177500fc90710e
Ty-Pope <138350487+Ty-Pope@users.noreply.github.com>
2024-06-17 12:14:29 -0600

    week 9 prepare

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,21 @@
+public class Shape
+{
+ private string color;
+
+ public Shape(string color)
+ {
+  this.color = color;
+ }
+ public string GetColor()
+ {
+  return color;
+ }
+ public void SetColor(string color)
+ {
+  this.color = color;
+ }
+ public virtual double GetArea()
+ {
+  return 0;
+ }
+}
